5b98c0ed2682252f87eb3352e374a4555d4b9199
Temidayo Ajisebutu <dayoajisebutu@gmail.com>
2018-04-06 23:16:00 +0100

    changed the file converter to single file only and also changed the promis function to a promise.

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 module.exports = function (event, vanilla = false) {
-  let reader = new FileReader();
-  let files = (vanilla === false) ? event.target.files : event.files,
-    len = files.length,
-    result = [];
-  if (files && files.length > 0) {
-    if (files.length > 1) {
-      for (let i = files.length; i > 0; i--) {
-        let file = files[i]
-        reader.readAsDataURL(file);
-        reader.onload = () => {
-          let base64 = reader.result.split(',')[1];
-          file.base64 = base64;
-          result.push(file);
-        };
-      }
-      return result;
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    let files = (vanilla === false) ? event.target.files : event.files;
+    let len = files.length;
+    if (len > 1) {
+      reject(new DOMException("Only one file can be uploaded at a time"));
     } else {
+      reader.onerror = () => {
+        reader.abort();
+        reject(new DOMException("Problem parsing input file."));
+      };
       let file = files[0]
-      reader.readAsDataURL(file);
       reader.onload = () => {
         let base64 = reader.result.split(',')[1];
         file.base64 = base64;
-        return file;
+        resolve(file);
       };
+      reader.readAsDataURL(file);
     }
-  }
+  });
 }
