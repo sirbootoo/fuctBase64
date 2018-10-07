b341a181ec09f156d1985bcc2f42827f75359bb9
Temidayo Ajisebutu <dayoajisebutu@gmail.com>
2018-10-07 13:12:09 +0100

    added binaryFileType to part of the repsonse provided by the library!

--- index.js
+++ index.js
@@ -11,12 +11,38 @@ module.exports = function (event) {
         reject(new DOMException("Problem parsing input file."));
       };
       let file = files[0]
-      reader.onload = () => {
+      reader.onload = (evt) => {
+        const uint = new Uint8Array(evt.target.result);
+        let bytes = [];
+        uint.map(byte => {
+          bytes.push(byte.toString(16));
+        });
+        const hex = bytes.join('').toUpperCase();
         let base64 = reader.result.split(',')[1];
         file.base64 = base64;
+        file.binaryFileType = getMimetype(hex);
         resolve(file);
       };
       reader.readAsDataURL(file);
     }
   });
+}
+
+
+const getMimetype = (signature) => {
+  switch (signature) {
+      case '89504E47':
+          return 'image/png'
+      case '47494638':
+          return 'image/gif'
+      case '25504446':
+          return 'application/pdf'
+      case 'FFD8FFDB':
+      case 'FFD8FFE0':
+          return 'image/jpeg'
+      case '504B0304':
+          return 'application/zip'
+      default:
+          return 'Unknown filetype'
+  }
 }
